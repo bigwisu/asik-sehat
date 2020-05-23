cfcf26de7d7d03d97c29ebb74d75c8aff61d21ce
Wisu Suntoyo <wisu@google.com>
2020-05-23 08:14:58 +0700

    remove src

--- src/actions/types.js
+++
@@ -1,9 +0,0 @@
-const actionTypes = {
-    FetchPosts: 'FETCH_POSTS',
-    IncrementLikes: 'INCREMENT_LIKES',
-    FetchComments: 'FETCH_COMMENTS',
-    AddComment: 'ADD_COMMENT',
-    RemoveComment: 'REMOVE_COMMENT'
-};
-
-export default actionTypes;
